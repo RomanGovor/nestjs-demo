a4f7f63dd23a5700f0382c7e557ba4ba831f03b7
Roman Govor <roman.govor@activeplatform.com>
2023-07-25 20:41:09 +0300

    feat: add product service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -4,31 +4,67 @@ import {
   Delete,
   Get,
   HttpCode,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
-import { ProductModel } from './product.model';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FindProductDto } from './dto/find-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
+import { CreateProductDto } from './dto/create-product.dto';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Post('create')
-  async create(@Body() dto: Omit<ProductModel, '_id'>) {}
+  async create(@Body() dto: CreateProductDto) {
+    return this.productService.create(dto);
+  }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  async get(@Param('id') id: string) {}
+  async get(@Param('id', IdValidationPipe) id: string) {
+    const product = await this.productService.findById(id);
+    if (!product) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+    return product;
+  }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    const deletedProduct = await this.productService.deleteById(id);
+    if (!deletedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+  }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async patch(
-    @Param('id') id: string,
-    @Body() dto: Omit<ProductModel, '_id'>,
-  ) {}
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: CreateProductDto,
+  ) {
+    const updatedProduct = await this.productService.updateById(id, dto);
+    if (!updatedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+    }
+    return updatedProduct;
+  }
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  @Post()
-  async find(@Body() dto: FindProductDto) {}
+  @Post('find')
+  async find(@Body() dto: FindProductDto) {
+    return this.productService.findWithReviews(dto);
+  }
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -17,6 +17,7 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserEmail } from '../decorators/user-email.decorator';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -30,7 +31,7 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deleteDoc = await this.reviewService.delete(id);
 
     if (!deleteDoc) {
@@ -40,7 +41,7 @@ export class ReviewController {
 
   @Get('byProduct/:productId')
   async getByProduct(
-    @Param('productId') productId: string,
+    @Param('productId', IdValidationPipe) productId: string,
     @UserEmail() email: string,
   ) {
     Logger.log(email);
@@ -49,7 +50,9 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Delete('byProduct/:productId')
-  async deleteByProductId(@Param('productId') productId: string) {
+  async deleteByProductId(
+    @Param('productId', IdValidationPipe) productId: string,
+  ) {
     return this.reviewService.deleteByProductId(productId);
   }
 }
